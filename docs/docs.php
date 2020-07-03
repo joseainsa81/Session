c70f7f9cc9c78750a7c8e6d3910afc0044ebed7b
joseainsa81 <37312767+joseainsa81@users.noreply.github.com>
2020-07-03 08:52:22 +0200

    Create docs.php

---
+++ docs/docs.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>Documentación</title>
+    <meta charset="utf-8" />
+    <style>
+        hr.sep {
+            margin: 40px 0 30px;
+        }
+
+        .generators>div {
+            float: left;
+            width: 50%;
+        }
+
+        h6 {
+            margin: 0 0 5px;
+        }
+
+        .generators iframe {
+            box-sizing: border-box;
+            width: 100%;
+            height: 500px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Documentación</h1>
+
+    <h3>phpDocumentor</h3>
+    <a href="phpdocumentor/clean/" target="_blank">clean</a><br />
+    <a href="phpdocumentor/responsive-twig/" target="_blank">responsive-twig</a>
+
+    <hr class="sep" />
+
+    <h2>Generación</h2>
+    <a href="phpdocumentor/generate.php" target="_blank">Generar documentación de phpdocumentor</a><br />
+</body>
+
+</html>
